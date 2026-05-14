e60bba1d072be96355198477033e06e25ca4fc56
Null-Corporation-alt <adrian.jimenez5326@gmail.com>
2026-05-14 17:28:50 +0200

    Delete preload.cjs

--- preload.cjs
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Aquí puedes exponer funciones del sistema a tu web de React de forma segura
-contextBridge.exposeInMainWorld('electronAPI', {
-  // Ejemplo: una función para cerrar la app si haces un botón personalizado
-  closeApp: () => ipcRenderer.send('close-app')
-});
